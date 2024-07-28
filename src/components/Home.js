72e228efbac519c8ef0872fed7c1f76e457715e3
onsra05 <hoanganh54321.oc@gmail.com>
2024-07-28 22:06:27 +0700

    a

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,9 @@ function Home() {
             <div classname="flex items-center space-x-4">
                 <h1 classname="text-[40] uppercase font-bold text-red-800">Movie</h1>
                 <nav classname="flex items-center space-x-4">
-                    <a href="https://www.youtube.com/watch?v=VLAlJ0v3aa4#" classname="text-white font-[20]">Home</a>
-                    <a href="https://www.youtube.com/watch?v=VLAlJ0v3aa4" classname="text-white font-[20]">About</a>
-                    <a href="https://www.youtube.com/watch?v=VLAlJ0v3aa4" classname="text-white font-[20]">Contacts</a>
+                    <a href="https://www.youtube. com/w" classname="text-white font-[20]">Home</a>
+                    <a href="https://www.youtube. com/wa0v3aa4" classname="text-white font-[20]">About</a>
+                    <a href="https://www.youtube. com/wat" classname="text-white font-[20]">Contacts</a>
                 </nav>
             </div>
             <div classname="flex items-center space-x-4 " >

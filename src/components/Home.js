fc8c260ac3d51de0406bc6eb12e65e7bf40ce5d2
onsra05 <hoanganh54321.oc@gmail.com>
2024-07-28 22:05:20 +0700

    1

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+function Home() {
+    return(
+        <div classname="p-4 bg-black flex items-center justify-between">
+            <div classname="flex items-center space-x-4">
+                <h1 classname="text-[40] uppercase font-bold text-red-800">Movie</h1>
+                <nav classname="flex items-center space-x-4">
+                    <a href="https://www.youtube.com/watch?v=VLAlJ0v3aa4#" classname="text-white font-[20]">Home</a>
+                    <a href="https://www.youtube.com/watch?v=VLAlJ0v3aa4" classname="text-white font-[20]">About</a>
+                    <a href="https://www.youtube.com/watch?v=VLAlJ0v3aa4" classname="text-white font-[20]">Contacts</a>
+                </nav>
+            </div>
+            <div classname="flex items-center space-x-4 " >
+                <input type="text" placeholder="Search"/>
+                <button classname="p-2 text-white bg-red-600">Search</button>
+            </div>
+            
+
+        </div>
+    )
+}
+export default Home;
